ee0f501e842581e10f11889e36f0a3ff022dcec0
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-07 00:29:07 +0530

    Add files via upload

---
+++ example2.java
@@ -0,0 +1,9 @@
+class example2
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20,c=30;
+        System.out.print("Sum of a, b and c is: ");
+        System.out.println(a+b+c);
+    }
+}
