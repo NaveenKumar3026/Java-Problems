2ea5fb93eeb62c826c5f409d05141a97d72796b7
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-09 12:55:05 +0530

    Add files via upload

---
+++ example20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class example20
+{
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the starting number: ");
+        int a= scan.nextInt();
+        System.out.print("Enter the ending number: ");
+        int b= scan.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
