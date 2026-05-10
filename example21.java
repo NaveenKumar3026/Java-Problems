e191a844d933f5991a14064760e85289a494dbad
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-10 23:40:09 +0530

    Add files via upload

---
+++ example21.java
@@ -0,0 +1,13 @@
+class example17
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=10;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
