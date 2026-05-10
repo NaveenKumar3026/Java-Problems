fac431128923b5bdc92aa93d44017870707b5799
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-10 23:44:11 +0530

    Add files via upload

---
+++ example26.java
@@ -0,0 +1,11 @@
+class example26
+{
+    public static void main(String args[])
+    {
+        System.out.println("Printing the 2 table:-");
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("2 x " +i +" = " +2*i);
+        }
+    }
+}
