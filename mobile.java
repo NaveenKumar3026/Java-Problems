700fc8bde90a37a2f732dfe5e7edce67ccbe3a77
Naveen Kumar <naveen3026mit@gmail.com>
2026-05-11 22:36:11 +0530

    Add files via upload

---
+++ mobile.java
@@ -0,0 +1,23 @@
+public class mobile
+{
+    void iphone()
+    {
+        System.out.println("This is iPhone");
+    }
+    void samsung()
+    {
+        System.out.println("This is Samsung");
+    }
+    void oppo()
+    {
+        System.out.println("This is Oppo");
+    }
+
+    public static void main(String args[])
+    {
+        mobile mob1 = new mobile();
+        mob1.iphone();
+        mob1.samsung();
+        mob1.oppo();
+    }
+}
